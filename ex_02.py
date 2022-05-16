39b33a38bf5424534c2de6c081e9e3d487fbfe70
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-16 18:46:43 -0500

    finished chapter2

---
+++ ex_02.py
@@ -0,0 +1,11 @@
+name = input('Enter your name: ')
+print('Hello', name)
+
+#-----------------------------------------------------------
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = float(hours)*float(rate)
+print('Pay:',pay) 
+
+#------------------------------------------------------------
+
