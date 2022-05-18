b61542eea6e93f7b20ab1b2b6a0b3f2bd9895fa0
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-18 11:42:51 -0500

    add one more

--- ex_02.py
+++ ex_02.py
@@ -25,4 +25,52 @@ for i in range (5):
 print('All done') """
 #alt+shift+A = coment multiple line in vs code
 
+#----------------------------------------------------
+
+""" x = input ('Enter your input')
+x=int(x) """
+""" if x < 2:
+    print('Small')
+elif x >10:
+    print('Medium')
+else :
+    print('Large')
+print('All done') """
+
+""" if x<10:
+    print('Small')
+elif x<20:
+    print('Medium')
+elif x<40:
+    print('Big')
+elif x<50:
+    print('Large')
+else :
+    print('Ginormous')
+print('All done') """
+
+x=input('Enter a number:')
+""" x='Hello'
+try:
+    y=int(x)
+except:
+    y = -1
+print('First', y)
+x=123
+try:
+    y=int(x)
+except:
+    y = -1
+print('Second', y) """
+
+try:
+    z = int(x)
+except:
+    z = -1
+if z > 0 :
+    print('Nice work')
+else :
+    print('Not a number')
+
+
 
