84b0d26b96e8456fd95942c4e3819b62319b560a
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-18 10:50:38 -0500

    add some example

--- ex_02.py
+++ ex_02.py
@@ -1,11 +1,28 @@
-name = input('Enter your name: ')
-print('Hello', name)
+
+#name = input('Enter your name: ')
+#print('Hello', name)
 
 #-----------------------------------------------------------
-hours = input('Enter Hours: ')
-rate = input('Enter Rate: ')
-pay = float(hours)*float(rate)
-print('Pay:',pay) 
+#hours = input('Enter Hours: ')
+#rate = input('Enter Rate: ')
+#pay = float(hours)*float(rate)
+#print('Pay:',pay) 
 
 #------------------------------------------------------------
+
+""" x = input('Enter your input')
+x=int(x)
+if x > 2:
+    print('Bigger than 2')
+    print('Still bigger')
+print('Done with 2')
+
+for i in range (5):
+    print(i)
+    if i > 2:
+        print('Bigger than 2')
+    print('Done with i', i)
+print('All done') """
+#alt+shift+A = coment multiple line in vs code
+
 
