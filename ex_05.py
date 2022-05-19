6d078447d5eeff37b5958e59a518d33a27f39c54
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-19 11:39:01 -0500

    add_05

---
+++ ex_05.py
@@ -0,0 +1,26 @@
+#learn about repeated steps
+
+""" n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+#print('Blastoff!')
+print(n) """
+#-----------------------------
+""" n = 0
+while n >= 0:
+    print('Lather')
+    print('Rinse')
+    #n = n-1
+    #print(n)
+print('Dry off!') """
+#----------------------------
+n = 0
+while True:
+    if n == 3:
+        break
+    print(n)
+    n = n + 1
+
+
+
