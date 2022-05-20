822ef679f64bd01cb1f1dddfe4bd302a5d53b3d5
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-20 16:30:11 -0500

    add_08

---
+++ ex_08.py
@@ -0,0 +1,29 @@
+#lists
+
+""" fruit = 'banana'
+x = fruit[1]
+print(x) """
+#---------- Concatenating list using + ------
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+t = [9, 41, 12, 3, 74, 15]
+print(t[:3])
+
+stuff = list()
+stuff.append('book')
+stuff.append(99)
+print(stuff)
+stuff.append('cookie')
+print(stuff)
+
+friends = ['John', 'Emily', 'Sally']
+friends.sort()
+print(friends)
+print(friends[1])
+
+
+
+
