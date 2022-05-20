b64f8f5da9c0e51f495a1c046b295fdf77b9f019
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-20 11:02:28 -0500

    finished_06

---
+++ ex_06.py
@@ -0,0 +1,27 @@
+#Looping through strings
+
+""" for n in 'banana':
+    print(n)
+
+fruit = 'banana'
+x = len(fruit)
+print(x)
+
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count) """
+
+word = 'bananana'
+i = word.find('na')
+print(i)
+
+str = 'X-DSPAM-Confidence: 0.8475'
+a = str.find(':')
+print(a)
+b = str[a+1:]
+print(b)
+value = float(b)
+print(value)
