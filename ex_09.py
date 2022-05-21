8c0a69c4dcf932fea304922b74eb56647d271218
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-21 12:39:47 -0500

    add_09

---
+++ ex_09.py
@@ -0,0 +1,36 @@
+#dictionaries
+
+""" purse = dict()
+purse['Money'] = 12
+purse['Candy'] = 3
+purse['food'] = 75
+print(purse)
+print(purse['Candy'])
+purse['Candy'] = purse['Candy'] + 2
+print(purse) """
+
+""" lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+lst[0]=23
+print(lst[0])
+print(lst)
+
+add =dict()
+add['age'] = 23
+add['course'] = 180
+print(add)
+add['age'] = 20
+print(add) """
+
+dict = {'Fri': 20, 'Thu': 6, 'Sat': 1}
+dict['Thu'] = 13
+dict['Sat'] = 2
+dict['Sun'] = 9
+print(dict)
+#--------- dictionaries: common application-------
+
+
+
+
