c3fc77ccf3f9bdbc31acaf10e57dbeb0c64dea73
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-21 15:02:05 -0500

    finished_09

--- ex_09.py
+++ ex_09.py
@@ -24,13 +24,40 @@ print(add)
 add['age'] = 20
 print(add) """
 
-dict = {'Fri': 20, 'Thu': 6, 'Sat': 1}
+""" dict = {'Fri': 20, 'Thu': 6, 'Sat': 1}
 dict['Thu'] = 13
 dict['Sat'] = 2
 dict['Sun'] = 9
-print(dict)
+print(dict) """
 #--------- dictionaries: common application-------
 
 
+""" counts = dict()
+names = ['cha', 'che', 'cho', 'chan']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+print(counts) """
 
+""" counts = {'quincy': 1, 'murgest': 42, 'beau': 1}
+print(counts.get('kris', 0)) """
+#------Dictionaries and loops---------
+
+""" counts = dict()
+print('Enter a line of text:')
+line = input('')
+words = line.split()
+print('Words:', words)
+print('Counting...')
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('Counts', counts) """
+
+""" counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key]) """
+#--------------Ex and finiushed --------------------
 
