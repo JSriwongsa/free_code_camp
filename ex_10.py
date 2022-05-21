f8cbf5f8a082557098a2cc848ab0f59805e32846
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-21 15:14:48 -0500

    add_ex_10

---
+++ ex_10.py
@@ -0,0 +1,18 @@
+#----- Tuples------
+
+""" x = ('Glenn', 'Sally', 'Joshep' )
+print(x[2])
+y = (1, 2, 9)
+print(y)
+print(max(y))
+for iter in y:
+    print(iter)
+
+d = dict()
+d['quincy'] = 1
+d['beau'] = 5
+d['kris'] = 9
+for (k,i) in d.items():
+    print(k, i) """
+
+
