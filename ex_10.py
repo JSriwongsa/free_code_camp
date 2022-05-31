4bdf9401f5c0356459f5223f914fc7c64a5dd9b8
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-30 21:00:39 -0500

    finished

--- ex_10.py
+++ ex_10.py
@@ -15,4 +15,25 @@ d['kris'] = 9
 for (k,i) in d.items():
     print(k, i) """
 
+#-----sorting lists of tuples-----
 
+fname = input("Enter the Filename: ")
+if len(fname) < 2:
+  fname = "clown.txt"
+
+try:
+  fhand = open(fname,"r")
+except:
+  print("Invalid Filename.Try again!")
+  quit()
+
+d = dict()
+for line in fhand:
+  line = line.rstrip()
+  words = line.split()
+
+  for word in words:
+    d[word] = d.get(word,0)+1
+
+common = sorted([(v,k) for k,v in d.items()],reverse=True)
+print(common[:5])
