3a1a0ebaaf7d2c59c28971ba38c20a3e855f5f88
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-06-02 13:44:37 -0500

    socket&urllib

---
+++ ex_12.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#----- networked programs -----
+""" import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))  """ #---- host + port ------
+
+#----- networking protocol & write a web browser-----
+
+""" import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+mysock.close() """
+
+# ----- Using urllib ----- it is shorter than using socket
+
+import urllib.request
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
+
